aede609b27bf1073a90278e051d17b45093f37ad
ethan7lesar <ethanlesar11@gmail.com>
2023-07-01 14:31:09 +0200

    first commit

---
+++ src/components/NavBarComp.vue
@@ -0,0 +1,178 @@
+<template>
+  <nav class="navbar bg-success sticky-top">
+    <div class="container-fluid">
+      <router-link to="/"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="100"
+          viewBox="0 0 120 120"
+          fill="none"
+        >
+          <g filter="url(#filter0_d_7_21)">
+            <path
+              d="M53.9635 50H57.2635V58.55H60.8035V50H64.1035V71H60.8035V61.55H57.2635V71H53.9635V50Z"
+              fill="black"
+            />
+            <path
+              d="M53.9635 50V49H52.9635V50H53.9635ZM57.2635 50H58.2635V49H57.2635V50ZM57.2635 58.55H56.2635V59.55H57.2635V58.55ZM60.8035 58.55V59.55H61.8035V58.55H60.8035ZM60.8035 50V49H59.8035V50H60.8035ZM64.1035 50H65.1035V49H64.1035V50ZM64.1035 71V72H65.1035V71H64.1035ZM60.8035 71H59.8035V72H60.8035V71ZM60.8035 61.55H61.8035V60.55H60.8035V61.55ZM57.2635 61.55V60.55H56.2635V61.55H57.2635ZM57.2635 71V72H58.2635V71H57.2635ZM53.9635 71H52.9635V72H53.9635V71ZM53.9635 51H57.2635V49H53.9635V51ZM56.2635 50V58.55H58.2635V50H56.2635ZM57.2635 59.55H60.8035V57.55H57.2635V59.55ZM61.8035 58.55V50H59.8035V58.55H61.8035ZM60.8035 51H64.1035V49H60.8035V51ZM63.1035 50V71H65.1035V50H63.1035ZM64.1035 70H60.8035V72H64.1035V70ZM61.8035 71V61.55H59.8035V71H61.8035ZM60.8035 60.55H57.2635V62.55H60.8035V60.55ZM56.2635 61.55V71H58.2635V61.55H56.2635ZM57.2635 70H53.9635V72H57.2635V70ZM54.9635 71V50H52.9635V71H54.9635Z"
+              fill="#C0C0C0"
+            />
+            <path
+              d="M41.2647 35H50.2647V38H44.5647V43.55H49.0947V46.55H44.5647V53H50.2647V56H41.2647V35Z"
+              fill="black"
+            />
+            <path
+              d="M41.2647 35V34H40.2647V35H41.2647ZM50.2647 35H51.2647V34H50.2647V35ZM50.2647 38V39H51.2647V38H50.2647ZM44.5647 38V37H43.5647V38H44.5647ZM44.5647 43.55H43.5647V44.55H44.5647V43.55ZM49.0947 43.55H50.0947V42.55H49.0947V43.55ZM49.0947 46.55V47.55H50.0947V46.55H49.0947ZM44.5647 46.55V45.55H43.5647V46.55H44.5647ZM44.5647 53H43.5647V54H44.5647V53ZM50.2647 53H51.2647V52H50.2647V53ZM50.2647 56V57H51.2647V56H50.2647ZM41.2647 56H40.2647V57H41.2647V56ZM41.2647 36H50.2647V34H41.2647V36ZM49.2647 35V38H51.2647V35H49.2647ZM50.2647 37H44.5647V39H50.2647V37ZM43.5647 38V43.55H45.5647V38H43.5647ZM44.5647 44.55H49.0947V42.55H44.5647V44.55ZM48.0947 43.55V46.55H50.0947V43.55H48.0947ZM49.0947 45.55H44.5647V47.55H49.0947V45.55ZM43.5647 46.55V53H45.5647V46.55H43.5647ZM44.5647 54H50.2647V52H44.5647V54ZM49.2647 53V56H51.2647V53H49.2647ZM50.2647 55H41.2647V57H50.2647V55ZM42.2647 56V35H40.2647V56H42.2647Z"
+              fill="#C0C0C0"
+            />
+            <path
+              d="M70.5899 60.7143H73.8899V78.7143H79.3199V81.7143H70.5899V60.7143Z"
+              fill="black"
+            />
+            <path
+              d="M70.5899 60.7143V59.7143H69.5899V60.7143H70.5899ZM73.8899 60.7143H74.8899V59.7143H73.8899V60.7143ZM73.8899 78.7143H72.8899V79.7143H73.8899V78.7143ZM79.3199 78.7143H80.3199V77.7143H79.3199V78.7143ZM79.3199 81.7143V82.7143H80.3199V81.7143H79.3199ZM70.5899 81.7143H69.5899V82.7143H70.5899V81.7143ZM70.5899 61.7143H73.8899V59.7143H70.5899V61.7143ZM72.8899 60.7143V78.7143H74.8899V60.7143H72.8899ZM73.8899 79.7143H79.3199V77.7143H73.8899V79.7143ZM78.3199 78.7143V81.7143H80.3199V78.7143H78.3199ZM79.3199 80.7143H70.5899V82.7143H79.3199V80.7143ZM71.5899 81.7143V60.7143H69.5899V81.7143H71.5899Z"
+              fill="#C0C0C0"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_7_21"
+              x="37.2646"
+              y="35"
+              width="46.0552"
+              height="54.7143"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="4" />
+              <feGaussianBlur stdDeviation="2" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0.360784 0 0 0 0 0.721569 0 0 0 0 0.360784 0 0 0 0.85 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_7_21"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_7_21"
+                result="shape"
+              />
+            </filter>
+          </defs></svg
+      ></router-link>
+    </div>
+    <div>
+      <button
+        class="btn align-items-end"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="60"
+          height="60"
+          viewBox="0 0 100 100"
+          fill="none"
+        >
+          <rect
+            x="1"
+            y="1"
+            width="98"
+            height="98"
+            rx="29"
+            fill="black"
+            stroke="#C0C0C0"
+            stroke-width="2"
+          />
+          <rect
+            x="22"
+            y="20"
+            width="55"
+            height="9"
+            fill="#5CB85C"
+            fill-opacity="0.85"
+          />
+          <rect
+            x="22"
+            y="45"
+            width="55"
+            height="9"
+            fill="#5CB85C"
+            fill-opacity="0.85"
+          />
+          <rect
+            x="22"
+            y="69"
+            width="55"
+            height="9"
+            fill="#5CB85C"
+            fill-opacity="0.85"
+          />
+        </svg>
+      </button>
+    </div>
+  </nav>
+  <div
+    class="offcanvas offcanvas-end bg-black w-50"
+    tabindex="-1"
+    id="offcanvasRight"
+    aria-labelledby="offcanvasRightLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+      <button
+        type="button"
+        class="close-button"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="55"
+          height="55"
+          viewBox="0 0 99 99"
+          fill="none"
+        >
+          <rect
+            x="13.2354"
+            y="5"
+            width="114"
+            height="20"
+            rx="10"
+            transform="rotate(41.4345 13.2354 5)"
+            fill="#5CB85C"
+          />
+          <rect
+            x="94.1812"
+            y="13.3904"
+            width="114"
+            height="20"
+            rx="10"
+            transform="rotate(132.03 94.1812 13.3904)"
+            fill="#C0C0C0"
+          />
+        </svg>
+      </button>
+    </div>
+    <div class="offcanvas-body">...</div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
