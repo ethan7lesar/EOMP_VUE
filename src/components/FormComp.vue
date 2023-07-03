3e5d0bd74c0f359ec2f35eaf86c7e635f5bd2856
ethan7lesar <ethanlesar11@gmail.com>
2023-07-03 15:54:01 +0200

    Form

---
+++ src/components/FormComp.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="m-2 p-3 contact mx-auto w-50">
+    <form action="https://formspree.io/f/xqkorznj" method="POST">
+      <h1 class="card p-3 text-center heading-text">Contact Us</h1>
+      <div class="mb-3 text-center">
+        <label class="form-label"> Full Name: </label>
+        <input
+          type="text"
+          class="form-control border-2"
+          name="Name"
+          placeholder="Your Full Name"
+          required
+        />
+      </div>
+
+      <div class="mb-3 text-center">
+        <label class="form-label"> Email: </label>
+        <input
+          type="email"
+          class="form-control border-2"
+          name="Email"
+          placeholder="Valid Email Address"
+          required
+        />
+      </div>
+
+      <div class="mb-3 text-center">
+        <label class="form-label"> Contact Number: </label>
+        <input
+          type="tel"
+          class="form-control border-2"
+          name="Contact"
+          placeholder="Mobile / Telephone Number"
+          required
+        />
+      </div>
+
+      <div class="mb-3 text-center">
+        <label class="form-label"> Leave Me a Message: </label>
+        <textarea
+          name="message"
+          class="form-control border-2"
+          rows="5"
+        ></textarea>
+      </div>
+
+      <button
+        type="submit"
+        class="btn text-center btn-submit"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+h1 {
+  font-size: 40px;
+}
+.contact {
+  border: 5px #5cb85c solid;
+  border-radius: 60px;
+  background-color: transparent;
+}
+.card {
+  background-color: transparent;
+  border: 0;
+  color: #5cb85c;
+}
+input {
+  background-color: #c0c0c0 !important;
+  color: black !important;
+}
+textarea {
+  background-color: #c0c0c0 !important;
+  color: black !important;
+}
+label {
+  font-size: 25px;
+  color: #5cb85c;
+}
+.btn.text-center.btn-submit{
+    border: 3px solid #5cb85c;
+    font-size: 30px;
+    color: #5cb85c;
+}
+
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <FormComp />
+  </div>
+</template>
+<script>
+import FormComp from "@/components/FormComp.vue";
+export default {
+  components: { FormComp },
+};
+</script>
