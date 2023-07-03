e7d471250c025871055c663a8acf4869d73b2c14
ethan7lesar <ethanlesar11@gmail.com>
2023-07-03 14:44:28 +0200

    v1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,125 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-text text-xl">Aspiring Entrepreneur and</div>
+    <div class="text">Developer</div>
+    <div class="d-flex justify-content-evenly">
+      <div class="logo text-start w-25">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="519"
+          height="327"
+          viewBox="0 0 519 327"
+          fill="none"
+        >
+          <g filter="url(#filter0_d_7_42)">
+            <path
+              d="M213.074 68.875H240.574V140.125H270.074V68.875H297.574V243.875H270.074V165.125H240.574V243.875H213.074V68.875Z"
+              fill="black"
+            />
+            <path
+              d="M213.074 68.875V61.875H206.074V68.875H213.074ZM240.574 68.875H247.574V61.875H240.574V68.875ZM240.574 140.125H233.574V147.125H240.574V140.125ZM270.074 140.125V147.125H277.074V140.125H270.074ZM270.074 68.875V61.875H263.074V68.875H270.074ZM297.574 68.875H304.574V61.875H297.574V68.875ZM297.574 243.875V250.875H304.574V243.875H297.574ZM270.074 243.875H263.074V250.875H270.074V243.875ZM270.074 165.125H277.074V158.125H270.074V165.125ZM240.574 165.125V158.125H233.574V165.125H240.574ZM240.574 243.875V250.875H247.574V243.875H240.574ZM213.074 243.875H206.074V250.875H213.074V243.875ZM213.074 75.875H240.574V61.875H213.074V75.875ZM233.574 68.875V140.125H247.574V68.875H233.574ZM240.574 147.125H270.074V133.125H240.574V147.125ZM277.074 140.125V68.875H263.074V140.125H277.074ZM270.074 75.875H297.574V61.875H270.074V75.875ZM290.574 68.875V243.875H304.574V68.875H290.574ZM297.574 236.875H270.074V250.875H297.574V236.875ZM277.074 243.875V165.125H263.074V243.875H277.074ZM270.074 158.125H240.574V172.125H270.074V158.125ZM233.574 165.125V243.875H247.574V165.125H233.574ZM240.574 236.875H213.074V250.875H240.574V236.875ZM220.074 243.875V68.875H206.074V243.875H220.074Z"
+              fill="#C0C0C0"
+            />
+            <path
+              d="M161.558 28H236.558V53H189.058V99.25H226.808V124.25H189.058V178H236.558V203H161.558V28Z"
+              fill="black"
+            />
+            <path
+              d="M161.558 28V21H154.558V28H161.558ZM236.558 28H243.558V21H236.558V28ZM236.558 53V60H243.558V53H236.558ZM189.058 53V46H182.058V53H189.058ZM189.058 99.25H182.058V106.25H189.058V99.25ZM226.808 99.25H233.808V92.25H226.808V99.25ZM226.808 124.25V131.25H233.808V124.25H226.808ZM189.058 124.25V117.25H182.058V124.25H189.058ZM189.058 178H182.058V185H189.058V178ZM236.558 178H243.558V171H236.558V178ZM236.558 203V210H243.558V203H236.558ZM161.558 203H154.558V210H161.558V203ZM161.558 35H236.558V21H161.558V35ZM229.558 28V53H243.558V28H229.558ZM236.558 46H189.058V60H236.558V46ZM182.058 53V99.25H196.058V53H182.058ZM189.058 106.25H226.808V92.25H189.058V106.25ZM219.808 99.25V124.25H233.808V99.25H219.808ZM226.808 117.25H189.058V131.25H226.808V117.25ZM182.058 124.25V178H196.058V124.25H182.058ZM189.058 185H236.558V171H189.058V185ZM229.558 178V203H243.558V178H229.558ZM236.558 196H161.558V210H236.558V196ZM168.558 203V28H154.558V203H168.558Z"
+              fill="#C0C0C0"
+            />
+            <path
+              d="M289.564 98.0714H317.064V248.071H362.314V273.071H289.564V98.0714Z"
+              fill="black"
+            />
+            <path
+              d="M289.564 98.0714V91.0714H282.564V98.0714H289.564ZM317.064 98.0714H324.064V91.0714H317.064V98.0714ZM317.064 248.071H310.064V255.071H317.064V248.071ZM362.314 248.071H369.314V241.071H362.314V248.071ZM362.314 273.071V280.071H369.314V273.071H362.314ZM289.564 273.071H282.564V280.071H289.564V273.071ZM289.564 105.071H317.064V91.0714H289.564V105.071ZM310.064 98.0714V248.071H324.064V98.0714H310.064ZM317.064 255.071H362.314V241.071H317.064V255.071ZM355.314 248.071V273.071H369.314V248.071H355.314ZM362.314 266.071H289.564V280.071H362.314V266.071ZM296.564 273.071V98.0714H282.564V273.071H296.564Z"
+              fill="#C0C0C0"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_7_42"
+              x="141.558"
+              y="28"
+              width="240.756"
+              height="285.071"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="20" />
+              <feGaussianBlur stdDeviation="10" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0.360784 0 0 0 0 0.721569 0 0 0 0 0.360784 0 0 0 1 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_7_42"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_7_42"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+      </div>
+      <div class="text-end pe-5 w-25">
+        <img
+          class="myphoto"
+          src="https://i.postimg.cc/JnZSMSHf/c12-c1-ethan-lesar-3-1.png"
+          alt="ethan"
+        />
+      </div>
+    </div>
+    <div class="mb-5">
+      <button class="learnmore">
+        <router-link to="/about" class="LM-btn"
+          >Click Here To Learn More</router-link
+        >
+      </button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script></script>
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style>
+.main-text {
+  color: #c0c0c0;
+  font-size: 70px;
 }
-</script>
+.text {
+  color: #c0c0c0;
+  font-size: 80px;
+}
+.LM-btn {
+  color: white;
+  border: 0;
+  text-decoration: none;
+  font-size: 30px;
+}
+.learnmore {
+  background-color: transparent;
+  border-radius: 30px;
+  border: #5cb85c 5px solid;
+  height: 75px;
+  width: 300px;
+}
+img{
+  position: relative;
+}
+
+</style>
