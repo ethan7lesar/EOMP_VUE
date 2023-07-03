3fa0c8718fbc859a05729ed22319a25710d0acd9
ethan7lesar <ethanlesar11@gmail.com>
2023-07-03 16:34:11 +0200

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="aboutme">About Me</h1>
+    <p>
+      My name is Ethan Henry Lesar. I am a man who grew up in a little
+      neighbourhood outside of town named Walmer Estate in Cape Town. The
+      neighbourhood is ethnically and religiously diverse. I am highly
+      enthusiastic about business. I got into coding because I previously worked
+      in application development, but owing to COVID, I opted to quit out. I
+      concluded that to be effective at business, I needed to learn about the
+      future, which is totally online, as well as the services and products that
+      are available online. As a result, I began to appreciate coding since I
+      now see the internet realm in a different way.
+    </p>
   </div>
 </template>
+<script></script>
+<style>
+.aboutme {
+  font-size: 50px;
+  color: #5cb85c;
+}
+p {
+  color: white;
+  font-size: 25px;
+}
+</style>
